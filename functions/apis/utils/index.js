0f860387f007d40ec5787c9633ec0b0b9c268e6d
Vinicius Kremer Santos <vinicius.kremer@gmail.com>
2023-07-31 21:01:33 -0300

    [AD-20] Iniciando implementação do utils

---
+++ functions/apis/utils/index.js
@@ -0,0 +1,12 @@
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const express = require("express");
+const cors = require("cors");
+
+// Variáveis utilitárias
+const db = admin.firestore();
+const logger = functions.logger;
+const app = express();
+
+// Automatically allow cross-origin requests
+app.use(cors({ origin: true }));
