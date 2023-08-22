2a9eb61a15c21418347de9271db915f64632e74c
Vinicius Kremer Santos <vinicius.kremer@gmail.com>
2023-08-21 21:26:01 -0300

    [AD-20] Estruturando função de upload de arquivos

--- functions/apis/files/index.js
+++ functions/apis/files/index.js
@@ -11,6 +11,28 @@ const app = express();
 // Automatically allow cross-origin requests
 app.use(cors({ origin: true }));
 
+app.post("/", (req, res) => {
+  logger.info("Iniciando o upload de arquivos.");
+  const body = req.body;
+  const bucket = admin.storage().bucket();
 
+  body.files.forEach((file) => {
+    let typeFile = file.type.includes("x-zip") ? "application/zip" : file.type;
+    let sizeBuffer = Buffer.from(file.size, "base64");
+    let sizeByteArray = new Uint8Array(sizeBuffer);
+    let newFile = bucket.file(`${body.path}/${file.name}`);
+    let options = {
+      resumable: false,
+      metadata: { contentType: typeFile },
+    };
+
+    newFile.save(sizeByteArray, options);
+  });
+
+  res.status(201).json({
+    message: "Upload realizado com sucesso!",
+    data: [],
+  });
+});
 
 exports.files = functions.https.onRequest(app);
--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,7 @@
 const admin = require("firebase-admin");
 const serviceAccount = require("./service-account/alegriatech.json");
 
-admin.initializeApp({ credential: admin.credential.cert(serviceAccount) });
+admin.initializeApp({ credential: admin.credential.cert(serviceAccount), storageBucket: "gs://alegriatech-2bf22.appspot.com" });
 
 const usersApi = require("./apis/users");
 const albumsApi = require("./apis/albums");
