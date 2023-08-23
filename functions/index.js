168594a4630866cc32731c3c64b4a5dc14a63ad0
Vinicius Kremer Santos <vinicius.kremer@gmail.com>
2023-08-23 18:50:54 -0300

    [FIX] Removendo files

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,15 @@
 const admin = require("firebase-admin");
 const serviceAccount = require("./service-account/alegriatech.json");
 
-admin.initializeApp({ credential: admin.credential.cert(serviceAccount), storageBucket: "gs://alegriatech-2bf22.appspot.com" });
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  storageBucket: "gs://alegriatech-2bf22.appspot.com",
+});
 
 const usersApi = require("./apis/users");
 const albumsApi = require("./apis/albums");
-const filesApi = require("./apis/files");
 
 module.exports = {
   ...usersApi,
   ...albumsApi,
-  ...filesApi,
 };
